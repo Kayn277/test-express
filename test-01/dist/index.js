a7e7f76b26a9b5fad81ab2dce20bf1c868b3edbc
kayn2282 <kayn2282@gmail.com>
2025-10-01 19:24:23 +0300

    added gitignore

--- test-01/dist/index.js
+++
@@ -1,8 +0,0 @@
-import { configDotenv } from "dotenv";
-import express from "express";
-import { env } from "./utils/env-validation.js";
-const app = express();
-app.listen(env.PORT, () => {
-    console.log("Listen on port", env.PORT);
-});
-//# sourceMappingURL=index.js.map
--- test-01/dist/utils/env-validation.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const env: {
-    PORT: number;
-};
-//# sourceMappingURL=env-validation.d.ts.map
